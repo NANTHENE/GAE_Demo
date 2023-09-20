97f6712a9e1578d85255c6d4b0f406e51be47edb
Nanthene R E <130129439+NANTHENE@users.noreply.github.com>
2023-09-20 05:35:40 +0530

    index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<html><head><title>Greeting</title></head><body><h1 style="color:blue;font-style:calibri">Google App Engine Demo</h1><p>Hello, World!</p></body></html>'
 
 if __name__ == "__main__":
    app.run()
